9e8b7501148fb293b17794efd72c07b15831b647
AntonioDusti1 <152974953+AntonioDusti1@users.noreply.github.com>
2024-01-24 17:45:43 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zadnji
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] niz1 = { 1, 2, 3, 4, 5 };
+            int[] niz2 = { 1, 2, 6, 4, 5 };
+            int koliko = 0;
+
+            for (int i = 0; i < niz1.Length; i++)
+            {
+                if (niz1[i] == niz2[i])
+                {
+                    koliko++;
+                }
+            }
+
+            Console.WriteLine("Broj elementa sa istom vrijednosti na isti niz je: " + koliko);
+            Console.ReadLine();
+        }
+    }
+}
